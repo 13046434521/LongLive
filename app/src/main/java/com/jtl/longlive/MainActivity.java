2302282be98068744f0573ce5a8f8d025bc20745
龙昊 <604872657@qq.com>
2020-11-02 15:59:16 +0800

    修改标题

--- app/src/main/java/com/jtl/longlive/MainActivity.java
+++ app/src/main/java/com/jtl/longlive/MainActivity.java
@@ -3,6 +3,7 @@ package com.jtl.longlive;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.jtl.ijklibrary.widget.media.VideoView;
@@ -13,31 +14,36 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = HomeActivity.class.getSimpleName();
     private VideoView mVideoView;
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mVideoView = findViewById(R.id.video_main_play);
-        Intent intent =getIntent();
+        mToolbar = findViewById(R.id.tool_main_show);
+        setSupportActionBar(mToolbar);
+        Intent intent = getIntent();
         String url = intent.getStringExtra("Url");
-        Log.d(TAG,"url:"+url);
+        String name = intent.getStringExtra("Name");
+        Log.d(TAG, "url:" + url);
         mVideoView.setUrl(url);
-//        mVideoView.setUrl("rtmp://58.200.131.2:1935/livetv/cctv4");
-        mVideoView.start();
+        mToolbar.setTitle(name);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mVideoView.canPause();
-        mVideoView.pause();
+        if (mVideoView.canPause()) {
+            mVideoView.pause();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mVideoView.resume();
+        mVideoView.start();
     }
 
     @Override
